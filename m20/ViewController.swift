6666bc633e382583f1f5f525b70c4d25afcf6f79
Roman Gnativ <gnativromaneducation@gmail.com>
2024-05-27 20:43:15 +0500

    begin

---
+++ m20/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m20
+//
+//  Created by Гнатив Роман on 27.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
